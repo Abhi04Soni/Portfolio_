4021e2255a6aeaf1f4d9bc3bcd52ece5f6420966
Abhi <soni5abhi23@gmail.com>
2023-11-20 00:14:36 +0530

    Changing some project details

--- portfolioLatest/src/Pages/Projects.jsx
+++ portfolioLatest/src/Pages/Projects.jsx
@@ -11,7 +11,7 @@ export default function Projects() {
       <h1>My Projects</h1>
       <div className="projects">
       <ProjectCard src={commingSoonImg} title={"Ecom Web Scrapper"} desc={"A app which allows the user to search for a particualr product on multiple sites"} />
-      <a href="https://react00chat00app.netlify.app/" target='_blank'><ProjectCard src={chatappImg} title={"React Realtime messaging app"} desc={"A unique way to chat"} /></a>
+      <a href="https://react00chat00app.netlify.app/" target='_blank'><ProjectCard src={chatappImg} title={"React messaging app"} desc={"A unique way to chat"} /></a>
       <a href="https://ornate-starship-e90145.netlify.app/" target='_blank'><ProjectCard src={notesImg} title = {"Notes App"} desc={"A Note taking app"} /></a>
       <a href="https://calcu-basic.netlify.app" target='_blank'><ProjectCard src={calcImg} title={"Calculator"} desc={"A app allows us to perform basic mathematical operations"} /></a>
       {/* <ProjectCard src={commingSoonImg} title={"Ecom Web Scrapper"} desc={"A app which allows the user to search for a particualr product on multiple sites"} /> */}
